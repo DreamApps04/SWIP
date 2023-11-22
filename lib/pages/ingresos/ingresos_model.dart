4579cb32273288258ad7a4a035c2332275dd77f8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-22 02:00:23 +0000

    Validación de campos no vacios en ingresos, se crea funcion que devuelve el nombre del mes de una fecha actual

--- lib/pages/ingresos/ingresos_model.dart
+++ lib/pages/ingresos/ingresos_model.dart
@@ -5,6 +5,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/form_field_controller.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'ingresos_widget.dart' show IngresosWidget;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,7 @@ class IngresosModel extends FlutterFlowModel<IngresosWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
   // State field(s) for DropMes widget.
   String? dropMesValue;
   FormFieldController<String>? dropMesValueController;
@@ -27,18 +29,46 @@ class IngresosModel extends FlutterFlowModel<IngresosWidget> {
   FocusNode? txtnomIngresoFocusNode;
   TextEditingController? txtnomIngresoController;
   String? Function(BuildContext, String?)? txtnomIngresoControllerValidator;
+  String? _txtnomIngresoControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Nombre de ingreso es requerido';
+    }
+
+    return null;
+  }
+
   // State field(s) for txtDescripcion widget.
   FocusNode? txtDescripcionFocusNode;
   TextEditingController? txtDescripcionController;
   String? Function(BuildContext, String?)? txtDescripcionControllerValidator;
+  String? _txtDescripcionControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Descripción es requerido';
+    }
+
+    return null;
+  }
+
   // State field(s) for txtMonto widget.
   FocusNode? txtMontoFocusNode;
   TextEditingController? txtMontoController;
   String? Function(BuildContext, String?)? txtMontoControllerValidator;
+  String? _txtMontoControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Monto es requerido';
+    }
+
+    return null;
+  }
 
   /// Initialization and disposal methods.
 
-  void initState(BuildContext context) {}
+  void initState(BuildContext context) {
+    txtnomIngresoControllerValidator = _txtnomIngresoControllerValidator;
+    txtDescripcionControllerValidator = _txtDescripcionControllerValidator;
+    txtMontoControllerValidator = _txtMontoControllerValidator;
+  }
 
   void dispose() {
     unfocusNode.dispose();
